653074f57f05af70ada5e764b7b3dde730dcf1e7
SanathNarayana <68953929+SanathNarayana@users.noreply.github.com>
2020-11-29 14:11:24 +0530

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -1,23 +1,27 @@
-class BaseClass{
+class BaseClass
+{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':0.8,
+            'restitution':0.3,
             'friction':1.0,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("sprites/base.png");
+        //this.image = loadImage("block1.png");
         World.add(world, this.body);
+        //this.Visibility=255;
       }
       display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
-        pop();
+        //var angle = this.boxObject.angle;
+        //push();
+        //translate(this.boxObject.position.x, this.boxObject.position.y);
+        //rotate(angle);
+      
+       // strokeWeight(1.5);
+       // rectMode(CENTER);
+       // rect(this.body.position.x,this.body.position.y, this.width, this.height);
+        
       }
 }
